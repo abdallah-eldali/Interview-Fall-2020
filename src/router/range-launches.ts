67c8fe974082e8cc7d54e6d325b86cf63785b099
Abdallah El Dali <abdallahed19@gmail.com>
2020-05-29 09:40:17 -0400

    Made the /yearly-launches route to go over all the years from 'start' to 'end' and request the API for the launch objects that were stored in an array

--- src/router/range-launches.ts
+++ src/router/range-launches.ts
@@ -1,24 +1,44 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
+import {Launches} from path.resolve(__dirname + "../src/spaceX/launches");
+
 //Handles the query parameter
-router.get("/", function(req, res, next){
+router.get("/", async function(req, res, next){
+    const yearly = new Launches();
+
     let start = req.query.start;
     let end = req.query.end;
 
     //parsing the dates into objects
     let objectStart = {
-        year: start.substring(0, 4),
-        month: start.substring(6, 8),
-        day: start.substring(9, 10)
+        year: Number(start.substring(0, 4)),
+        month: Number(start.substring(6, 8)),
+        day: Number(start.substring(9, 10))
     }
 
     let objectEnd = {
-        year: end.substring(0, 4),
-        month: end.substring(6, 8),
-        day: end.substring(9, 10)
+        year: Number(end.substring(0, 4)),
+        month: Number(end.substring(6, 8)),
+        day: Number(end.substring(9, 10))
     }
+
+    //Get the the difference in years between 'start' and 'end'
+    let lentgh = objectEnd.year - objectStart.year; 
+    //Create an empty Array where where the launch objects will be stored
+    let launches = [];
+    //Request the API for every launch from start to end
+    for (let i = 0; i < lentgh; ++i){
+        //Request for the launches from the start day all the way up to the end date
+        const result = await yearly.getLaunchesByYear((start.year + 1).toString());
+        launches.push(result);
+    }
+
+    console.log(launches);
+    
+    res.send(launches);
 });
 
 
